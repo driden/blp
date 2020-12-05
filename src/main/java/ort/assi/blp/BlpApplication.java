4fa81ef6a3a9f5178c5f8c445609e058c36d1e44
Francisco Bergeret <fbergeretf@gmail.com>
2020-12-05 12:04:21 -0300

    FileHandler

--- src/main/java/ort/assi/blp/BlpApplication.java
+++ src/main/java/ort/assi/blp/BlpApplication.java
@@ -3,6 +3,7 @@ package ort.assi.blp;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ort.assi.blp.handlers.FileHandler;
 
 @SpringBootApplication
 public class BlpApplication implements CommandLineRunner {
@@ -14,6 +15,6 @@ public class BlpApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		System.out.println("HOLAAA");
+		System.in.read();
 	}
 }
