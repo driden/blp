2684078be31bfca5ac907ac7bbee33ffa515b070
driden <driden_87@hotmail.com>
2020-12-06 18:09:03 -0300

    instructions

--- secure parte-2/src/main/java/ort/assi/blp/secure/SecureSystem.java
+++ secure parte-2/src/main/java/ort/assi/blp/secure/SecureSystem.java
@@ -3,13 +3,13 @@ package ort.assi.blp.secure;
 import ort.assi.blp.covertchannel.SequenceHandler;
 import ort.assi.blp.entities.SysObject;
 import ort.assi.blp.entities.SysSubject;
-import ort.assi.blp.io.instruction.CreateInstruction;
-import ort.assi.blp.io.instruction.Instruction;
+import ort.assi.blp.io.instruction.*;
 import ort.assi.blp.io.InstructionObject;
 
 import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.HashMap;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class SecureSystem {
@@ -41,16 +41,17 @@ public class SecureSystem {
         System.out.println(instruction.getMessage());
         System.out.println("The current state is:");
         referenceMonitor.getAllObjects().forEach(object -> System.out.println("\t" + object.getStatusMessage()));
-        referenceMonitor.getAllSubjects().forEach(subject -> System.out.println("\t" +subject.getStatusMessage()));
+        referenceMonitor.getAllSubjects().forEach(subject -> System.out.println("\t" + subject.getStatusMessage()));
         System.out.println();
     }
 
     public void run() {
         var hal = getSubject("hal");
         var lyle = getSubject("lyle");
+
         char c;
-        while ((c = sequenceHandler.getNextSubject())!= ' '){
-            switch (c){
+        while ((c = sequenceHandler.getNextSubject()) != ' ') {
+            switch (c) {
                 case 'H':
                     var createResult = referenceMonitor.executeInstruction(
                             new CreateInstruction(hal, new SysObject("objSync")));
@@ -58,8 +59,39 @@ public class SecureSystem {
                     break;
                 case 'L':
                     break;
-                default:;
+                default:
+                    ;
             }
         }
+    }
+
+
+    public Instruction[] sendBit0() {
+        var hal = getSubject("hal");
+
+        return new Instruction[]{
+                new RunInstruction(hal),
+                new CreateInstruction(hal, new SysObject("obj"))
+        };
+    }
+
+    public Instruction[] sendBit1() {
+        var hal = getSubject("hal");
+
+        return new Instruction[]{
+                new RunInstruction(hal),
+        };
+    }
+
+    public Instruction[] readBit() {
+        var lyle = getSubject("lyle");
+        SysObject obj = new SysObject("obj");
+        return new Instruction[]{
+                new CreateInstruction(lyle, obj),
+                new WriteInstruction(lyle, obj, 1),
+                new DestroyInstruction(lyle, obj),
+                new RunInstruction(lyle),
+                new CreateInstruction(lyle, obj),
+        };
     }
 }
